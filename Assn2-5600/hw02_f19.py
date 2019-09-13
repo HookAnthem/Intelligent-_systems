4a2708662cb971214b78e7b71caf2064927fe8e9
episong <zamhook@gmail.com>
2019-09-12 23:58:34 -0600

    Training layer completed

--- Assn2-5600/hw02_f19.py
+++ Assn2-5600/hw02_f19.py
@@ -19,24 +19,27 @@ def load(file_name):
         nn = pickle.load(fp)
     return nn
 
+def sigmoid(x, deriv=False):
+    if (deriv == True):
+        return np.exp(-x)/((1+np.exp(-x)**2))
+    return 1 / (1+np.exp(-x))
+
 def build_nn_wmats(mat_dims):
     np.random.seed(1)
     wmats_result = [None]*(len(mat_dims)-1)
     arrayitter = 0
     for i, j in zip(mat_dims[:-1], mat_dims[1:]):
-        #print(i)
         temp = np.random.rand(i,j)
-       # print(temp)
         wmats_result[arrayitter] = temp
         arrayitter= arrayitter + 1
     return wmats_result
 def build_241_nn():
     return build_nn_wmats((2,4,1))
-def build_184_nn():
-    return build_nn_wmats((1,8,4))
+def build_284_nn():
+    return build_nn_wmats((2,8,4))
 
-def build_91083_nn():
-    return build_nn_wmats((9,10,8,3))
+def build_2683_nn():
+    return build_nn_wmats((2,6,8,3))
 def build_2542_nn():
     return build_nn_wmats((2,5,4,2))
 
@@ -46,21 +49,44 @@ def build_2542_nn():
 ## build is a nn builder function.
 def train_3_layer_nn(numIters, X, y, build):
     W1, W2 = build()
-    
-    ## your code here
+    for i in range(numIters):
+        Z2 = np.dot(X, W1)
+        layer1 = sigmoid(Z2)
+        output = sigmoid(np.dot(layer1, W2))
+        output_error = y - output
+        output_delta = output_error * sigmoid(output, deriv=True)
+        layer1_error = output_delta.dot(W2.T)
+        layer1_delta = layer1_error * sigmoid(layer1, deriv=True)
+        W2 += layer1.T.dot(output_delta)
+        W1 += X.T.dot(layer1_delta)
     return W1, W2
 
-xor_wmats_184 = train_3_layer_nn(100,X1,y_xor,build_184_nn)
+
+xor_wmats_184 = train_3_layer_nn(100,X1,y_xor,build_284_nn)
 xor_wmats_241 = train_3_layer_nn(100,X1,y_xor,build_241_nn)
 
 def train_4_layer_nn(numIters, X, y, build):
     W1, W2, W3 = build()
-    ## your code here.    
+    for i in range(numIters):
+        Z2 = np.dot(X, W1)
+        layer1 = sigmoid(Z2)
+        layer2 = sigmoid(np.dot(layer1, W2))
+        output = sigmoid(np.dot(layer2, W3))
+        output_error = y - output
+        output_delta = output_error * sigmoid(output, deriv=True)
+        layer2_error = output_delta.dot(W3.T)
+        layer2_delta = layer2_error * sigmoid(layer2, deriv=True)
+        layer1_error = layer2.dot(W2.T)
+        layer1_delta = layer1_error * sigmoid(layer1, deriv=True)
+        W3 += layer2.T.dot(output_delta)
+        W2 += layer1.T.dot(layer2_delta)
+        W1 += X.T.dot(layer1_delta)
     return W1, W2, W3
 
 
 xor_wmats_2542 = train_4_layer_nn(100,X1,y_xor,build_2542_nn)
-xor_wmats_91083 = train_4_layer_nn(100,X1,y_xor,build_91083_nn)
+xor_wmats_91083 = train_4_layer_nn(100,X1,y_xor,build_2683_nn)
+
 def fit_3_layer_nn(x, wmats, thresh=0.4, thresh_flag=False):
     # your code here
     pass
